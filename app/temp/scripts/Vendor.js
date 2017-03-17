78cc91e930bf2349ce3ed8c2a760180a20f3fe0e
edwinchen85 <edwinchen85@gmail.com>
2017-03-17 18:50:51 +0800

    Add picturefill plugin for web browsers that don't support picture element and srcset attribute

--- app/temp/scripts/Vendor.js
+++ app/temp/scripts/Vendor.js
@@ -771,6 +771,8 @@
 "use strict";
 
 
+__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"picturefill\""); e.code = 'MODULE_NOT_FOUND';; throw e; }()));
+
 __webpack_require__(6);
 
 /***/ })
